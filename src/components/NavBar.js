d774190caa8bd25ab5315f691b19a71093ff16ae
Sean Oh <ohsean812@gmail.com>
2022-07-20 15:05:33 -0400

    Done with lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,14 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linksDisplay = links.map((link) => {
+    return <a href={`#${link}`} key={link}>{link}</a>
+  })
+
+  return <nav>
+    {/* display an <a> tag for each link here */}
+    {linksDisplay}
+    </nav>;
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,22 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+
+  // console.log(technologies)
+  
+  const techItem = technologies.map((techItem) => {
+    return <span key={techItem}>{techItem}</span>
+  })
+
+  // console.log(techItem)
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {/* render a <span> for each technology in the technologies array */
+        techItem}
       </div>
     </div>
   );
